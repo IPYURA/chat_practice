d4bb7906f1f98de76b61faa356fcf1f7d99b3150
IPYURA <0207ehdrjs@naver.com>
2025-06-15 20:31:37 +0900

    fastapi server fetch test

--- app/menu1/page.js
+++ app/menu1/page.js
@@ -1,6 +1,29 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
 
 const Menu1 = () => {
+  const [saveVal, setSaveVal] = useState(null);
+
+  useEffect(() => {
+    fetchApiData();
+  }, []);
+
+  useEffect(() => {
+    console.log("updated saveVal: ", saveVal);
+  }, [saveVal]);
+
+  const fetchApiData = async () => {
+    try {
+      const res = await fetch("http://34.47.116.47/health");
+      if (!res.ok) throw new Error("Network response was not ok");
+      const data = await res.json(); // 또는 await res.text() 등
+      setSaveVal(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return <div>This is Menu1 Page...</div>;
 };
 
